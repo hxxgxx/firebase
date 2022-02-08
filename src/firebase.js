ead58fe896c8c9fe4cc09666c11224f5e58ccd6e
hxxgxx <123408hi@naver.com>
2022-02-09 00:25:44 +0900

    firebase connect

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import * as firebase from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyD94eRT7iTtM8qvJ0lwHG8lnbKoHUvl5Go",
+    authDomain: "fir-2784b.firebaseapp.com",
+    projectId: "fir-2784b",
+    storageBucket: "fir-2784b.appspot.com",
+    messagingSenderId: "409355005177",
+    appId: "1:409355005177:web:548d55defb05dd665f0628"
+  };
+
+export default firebase.initializeApp(firebaseConfig);
